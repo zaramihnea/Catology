75a958ae7cab350f01ed65a9665ca932c2ba0d45
Zara Mihnea <zaramihnea@icloud.com>
2024-10-11 15:44:15 +0300

    Simple react front-end

---
+++ catology-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CatsologyApp from './catponents/CatologyApp';
+
+function App() {
+  return (
+    <div className="App">
+      <CatsologyApp />
+    </div>
+  );
+}
+
+export default App;
